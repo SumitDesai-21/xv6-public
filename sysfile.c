d1848fdb1830c50480c3489d637ccb31313c88fa
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-08-26 10:05:07 +0530

    Add lseek system call

--- sysfile.c
+++ sysfile.c
@@ -442,3 +442,46 @@ sys_pipe(void)
   fd[1] = fd1;
   return 0;
 }
+
+int
+sys_lseek(void)
+{
+  struct file *f;
+  int offset, whence;
+  long long newoff;
+
+  if(argfd(0, 0, &f) < 0 ||
+     argint(1, &offset) < 0 ||
+     argint(2, &whence) < 0)
+    return -1;
+
+  if(whence != SEEK_SET && whence != SEEK_CUR && whence != SEEK_END)
+    return -1;
+
+  if(f->type != FD_INODE)
+    return -1;
+
+  ilock(f->ip);
+
+  if(f->ip->type != T_FILE){
+    iunlock(f->ip);
+    return -1;
+  }
+
+  if(whence == SEEK_SET)
+    newoff = offset;
+  else if(whence == SEEK_CUR)
+    newoff = (long long)f->off + offset;
+  else
+    newoff = (long long)f->ip->size + offset;
+
+  if(newoff < 0 || newoff > f->ip->size){
+    iunlock(f->ip);
+    return -1;
+  }
+
+  f->off = (uint)newoff;
+
+  iunlock(f->ip);
+  return f->off;
+}
--- user.h
+++ user.h
@@ -9,6 +9,7 @@ int pipe(int*);
 int write(int, const void*, int);
 int read(int, void*, int);
 int close(int);
+int lseek(int, int, int);
 int kill(int);
 int exec(char*, char**);
 int open(const char*, int);
